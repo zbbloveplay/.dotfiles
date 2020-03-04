b7834c550e80394b3474496811f1be9788a877c7
Bangbo Zheng <zbbzucc@gmail.com>
2020-03-04 09:15:04 +0000

    small fixed

--- install/install.sh
+++ install/install.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
 
 # pip
-getpip() {
-  curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-  if [ which python >/dev/null 2>&1 ]; then
-    python get-pip.py
-  fi
-  if [ which python3 >/dev/null 2>&1 ]; then
-    python3 get-pip.py
-  fi
-}
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+if [ which python >/dev/null 2>&1 ]; then
+  python get-pip.py
+fi
+if [ which python3 >/dev/null 2>&1 ]; then
+  python3 get-pip.py
+fi
 
-# fzf
 getfzf() {
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 }
+
+getohmyzsh(){
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
+
+getnvm() {
+  wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+}
+
+
